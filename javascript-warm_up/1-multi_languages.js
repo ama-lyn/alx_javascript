d49d6a6e05accc2ed2441e9e5b871fb5ffe80c75
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-04 16:36:26 +0300

    Multi-languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+/*
+A script that prints 3 lines:
+   -use console.log(...) to print all output
+   -you are not allowed to use var */
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
