ce505301995880bc87c1180d044e1d6577f588a9
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-04 16:37:36 +0300

    Update

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
-/*
-A script that prints 3 lines:
-   -use console.log(...) to print all output
-   -you are not allowed to use var */
+/*A script that prints 3 lines:*/
 
 console.log("C is fun");
 console.log("Python is cool");
