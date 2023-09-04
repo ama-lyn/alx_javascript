6571d770d5041c4524f26477550ad7714fb51ead
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-04 18:10:08 +0300

    Add

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+/*A function that returns the addition of 2 integers.
+The function must be visible from outside*/
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  add: add,
+};
