3fff584618adb70a0879c11b20b1db98cbfba2d6
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-04 17:49:44 +0300

    Object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,5 +1,4 @@
-/*Update this script to replace the value 12 with 89:
-You are not allowed to use var */
+/*Update this script to replace the value 12 with 89:*/
 
 const myObject = {
   type: "object",
