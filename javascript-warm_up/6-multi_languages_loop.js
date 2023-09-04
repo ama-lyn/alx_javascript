2edf95736fd2008f39428133ca76bc410b2935c5
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-04 17:32:56 +0300

    Looping an array

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,15 @@
+/*Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output
+You are not allowed to use var
+You are not allowed to use any if/else statement
+You can use only one console.log
+You must use a loop (while, for, etc.) */
+
+const statement = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < statement.length; i++) {
+  console.log(statement[i]);
+}
