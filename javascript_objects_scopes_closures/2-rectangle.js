4ca1fd296506acf37cb2e87f324e70c131101751
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-05 23:39:01 +0300

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -12,10 +12,6 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      // Create an empty object
-      this.width = undefined;
-      this.height = undefined;
     }
   }
 }
