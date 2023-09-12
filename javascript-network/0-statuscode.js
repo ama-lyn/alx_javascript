f2c3d0c98e17554f85a260a629b94b4ea195d273
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-12 18:11:45 +0300

    Status  Code

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*Write a script that display the status code of a GET request.
+
+The first argument is the URL to request (GET)
+The status code must be printed like this: code: <status code>
+You must use the module request (NB: It must be installed if you haven’t already installed it) */
+
+const req = require("request");
+
+const url = `${process.argv[2]}`;
+
+req.get(url, { encoding: "utf-8" }).on("response", function (res) {
+  console.log(`code:${res.statusCode}`);
+});
