20dab980349fec0110e63f9202f11b4f806346fa
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-12 20:50:39 +0300

    Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -6,10 +6,11 @@ The first argument is the movie ID
 You must use the Star wars API with the endpoint https://swapi-api.alx-tools.com/api/films/:id
 You must use the module request*/
 
-const req = require("request");
-const ID = `${process.argv[2]}`;
-const url = `https://swapi-api.alx-tools.com/api/films/${ID}`;
+const req = require("request"); // Import the 'request' module.
+const ID = `${process.argv[2]}`; // Get the movie ID
+const url = `https://swapi-api.alx-tools.com/api/films/${ID}`; //Star Wars API endpoint
 
+//// Make a GET request to the API
 req(url, (error, response, body) => {
   if (error) {
     return console.error(error);
