9bbb99e85680fbb7b7c67f53442697964cc3d180
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-13 00:33:33 +0300

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -7,7 +7,7 @@ const req = require("request"); // Import the 'request' module.
 // Get the movie ID
 const url = `${process.argv[2]}`; //Star Wars API endpoint
 
-//// Make a GET request to the API
+// Make a GET request to the API
 req(url, (error, response, body) => {
   if (error) {
     return console.error(error);
@@ -18,10 +18,12 @@ req(url, (error, response, body) => {
   let count = 0;
   const characterId = "18"; // Character ID for Wedge Antilles
   const character = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
+
   for (const film of films) {
-    if (films.characters.includes(character)) {
+    if (film.characters.includes(character)) {
       count += 1;
     }
   }
+
   console.log(count);
 });
