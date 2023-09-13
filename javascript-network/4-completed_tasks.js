0dda2852fff2b6bb6e41e1f8a845de260789c08d
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-09-13 16:37:21 +0300

    Update

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -7,29 +7,26 @@ const req = require("request");
 
 const url = `${process.argv[2]}`;
 
-req(url, (error, body) => {
-  if (error) {
-    return console.error(error);
-  }
+if (process.argv.length > 2) {
+  req(url, function (error, response, body) {
+    if (error) {
+      return console.error(error);
+    }
 
-  if (response.statusCode !== 200) {
-    console.error(`Request failed with status code: ${response.statusCode}`);
-    return;
-  }
+    const todos = JSON.parse(body);
+    const completedTasks = {};
 
-  const todos = JSON.parse(body);
-  const completedTasks = {};
-
-  // Loop through the data to count completed tasks by user
-  for (const todo of todos) {
-    if (todo.completed) {
-      const userId = todo.userId.toString();
-      if (completedTasks[userId]) {
-        completedTasks[userId] += 1;
-      } else {
-        completedTasks[userId] = 1;
+    // Loop through the data to count completed tasks by user
+    for (const todo of todos) {
+      if (todo.completed) {
+        const userId = todo.userId.toString();
+        if (completedTasks[userId]) {
+          completedTasks[userId] += 1;
+        } else {
+          completedTasks[userId] = 1;
+        }
       }
     }
-  }
-  console.log(completedTasks);
-});
+    console.log(completedTasks);
+  });
+}
